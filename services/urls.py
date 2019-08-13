5ba67c7c4ba1d1682f0ca620181b9f5dc27871fa
sjy9412 <sjy9412@gmail.com>
2019-08-13 17:59:23 +0900

    실습 및 workshop

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('artii/', views.artii),
+    path('result/', views.result),
+    path('push/', views.push),
+    path('pull/', views.pull)
 ]
--- services/views.py
+++ services/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
     return render(request, 'services/index.html')
+
+def artii(request):
+    return render(request, 'services/artii.html')
+
+def result(request):
+    text = request.GET.get('text')
+    font = request.GET.get('font')
+    response = requests.get(f'http://artii.herokuapp.com/make?text={text}&font={font}')
+    result = response.text
+    context = {'result': result}
+    return render(request, 'services/result.html', context)
+
+def push(request):
+    return render(request, 'services/push.html')
+
+def pull(request):
+    number = request.GET.get('number')
+    context = {'number': number}
+    return render(request, 'services/pull.html', context)
